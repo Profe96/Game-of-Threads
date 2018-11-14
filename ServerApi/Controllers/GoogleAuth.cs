a5d7acf5d63ffbe0e6a2f0d5fc934403254639a0
david.alvarado <david.alvarado@alumnos.uneatlantico.es>
2018-11-14 18:20:10 +0100

    Server with Google Auth

--- ServerApi/Controllers/GoogleAuth.cs
+++ ServerApi/Controllers/GoogleAuth.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-using System.Web;
-
-using System.Net.Http;
-
-using Google.Apis.Oauth2.v2;
 using Google.Apis.Auth;
+
+using ServerApi.Models;
 
 namespace ServerApi.Controllers
 {
@@ -17,16 +14,20 @@ namespace ServerApi.Controllers
     [ApiController]
     public class GoogleAuth : Controller
     {
-        [HttpPost]
-        public void Post([FromBody] string token)
+        [HttpGet]
+        public async Task<ActionResult<string>> GetAsync(string idToken)
         {
-            authenticateToken(token);
+            User user = await getAuthorization(idToken);
+            return "logged";
         }
 
-
-        private async Task<GoogleJsonWebSignature.Payload> authenticateToken(string token)
+        public async Task<User> getAuthorization(string token)
         {
-            return await GoogleJsonWebSignature.ValidateAsync(token);
+            var validPayload = await GoogleJsonWebSignature.ValidateAsync(token);
+            return new User {
+                email = validPayload.Email,
+                authToken = token,
+            };
         }
     }
 }
--- game-of-threads-client/src/App.js
+++ game-of-threads-client/src/App.js
@@ -1,27 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    (function () {
+      var e = document.createElement("script");
+      e.async = true;
+      e.defer = true;
+      e.src = "https://apis.google.com/js/client:platform.js?onload=gPOnLoad";
+      var t = document.getElementsByTagName("script")[0];
+      t.parentNode.insertBefore(e, t)
+    })();
+  }
+
+  googleLogin = () => {
+    window.gapi.auth.signIn({
+      callback: function (authResponse) {
+        this.googleSignInCallback(authResponse)
+      }.bind(this),
+      clientid: "292122738397-rabof0ms7ocsb53k6kt23gg0aoqillur.apps.googleusercontent.com",
+      cookiepolicy: "single_host_origin",
+      requestvisibleactions: "http://schema.org/AddAction",
+      scope: "profile email"
+    });
+  }
+
+  googleSignInCallback = (e) => {
+    if (e["status"]["signed_in"]) {
+      window.gapi.client.load("plus", "v1", function () {
+        let token = e["access_token"];
+        if (token) {
+          this.getUserGoogleProfile(token)
+        }
+      }.bind(this));
+    }
+  }
+
+  getUserGoogleProfile = (accesstoken) => {
+    var e = window.gapi.client.plus.people.get({
+      userId: "me"
+    });
+    e.execute(function (e) {
+      if (e.id) {
+        console.log(accesstoken)
+        console.log(e.email);
+      }
+    }.bind(this));
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <div onClick={() => this.googleLogin()}>Google</div>
+    )
   }
 }
 
